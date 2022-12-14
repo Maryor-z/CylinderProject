f59e904394457cff87fa0c28b9955b234f2d2832
Maryor-z <88011448+Maryor-z@users.noreply.github.com>
2022-12-14 11:56:07 +0100

    Update Cylinder.java

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Cylinder {
+public class Cylinder1 extends Circle {
+
+    private double height, radius, volume;
+
+    public Cylinder1(double radius, double height) {
+        super(radius);
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume(){
+
+        return (getArea()*height);
+    }
 }
